15e2764f472ccaaf0f182fd428c408ff8599b45f
Pavel Priklonskiy <votymu@yandex.ru>
2023-10-24 15:40:07 +0300

    Add main8_2.c

---
+++ main8_2.c
@@ -0,0 +1,16 @@
+void sort_even_odd(int size, int a[])
+{
+    int tmp;
+    for (int i = 0; i < size / 2; i++)
+    {
+        for (int j = 0; j < size - 1; j++)
+        {
+            tmp = a[j];
+            if (tmp % 2 != 0 && a[j + 1] % 2 == 0)
+            {
+                a[j] = a[j + 1];
+                a[j + 1] = tmp;
+            }
+        }
+    }
+}
